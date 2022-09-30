064a293d921adf3372d3d15daa2c4897b0561fcc
Christian SB <christiansb.kodehode@gmail.com>
2022-09-30 22:19:12 +0200

    Added header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/side-shot.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Christian Sandum Breivik</h1>
+        <h5 className="text-light">Front End Developer</h5>
+        <CTA />
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll_down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
---
+++ src/components/header/HeaderSocial.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HeaderSocial = () => {
+  return (
+    <div className="header_socials">
+      <a href="https://www.linkedin.com/in/chris-sabre/" target="_blank"></a>
+      <a href="https://github.com/ChristianSB89" target="_blank"></a>
+      <a href="https://twitter.com/ChrisSanBrei" target="_blank"></a>
+    </div>
+  );
+};
+
+export default HeaderSocial;
