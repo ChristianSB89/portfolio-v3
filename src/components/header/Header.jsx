345567a11e39b42e0fabef21b0ada369af2fe296
Christian SB <christiansb.kodehode@gmail.com>
2022-09-30 22:59:15 +0200

    Fixed media queries on header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./header.css";
 import CTA from "./CTA";
 import ME from "../../assets/side-shot.png";
+import HeaderSocial from "./HeaderSocial";
 
 const Header = () => {
   return (
@@ -11,6 +12,7 @@ const Header = () => {
         <h1>Christian Sandum Breivik</h1>
         <h5 className="text-light">Front End Developer</h5>
         <CTA />
+        <HeaderSocial />
         <div className="me">
           <img src={ME} alt="me" />
         </div>
--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
 
 const HeaderSocial = () => {
   return (
     <div className="header_socials">
-      <a href="https://www.linkedin.com/in/chris-sabre/" target="_blank"></a>
-      <a href="https://github.com/ChristianSB89" target="_blank"></a>
-      <a href="https://twitter.com/ChrisSanBrei" target="_blank"></a>
+      <a href="https://www.linkedin.com/in/chris-sabre/" target="_blank">
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/ChristianSB89" target="_blank">
+        <FaGithubSquare />
+      </a>
+      <a href="https://twitter.com/ChrisSanBrei" target="_blank">
+        <FaTwitterSquare />
+      </a>
     </div>
   );
 };
