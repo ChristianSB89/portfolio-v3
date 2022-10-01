dd822c26ffe465757489fa69d44b9e6a10b91616
Christian SB <christiansb.kodehode@gmail.com>
2022-10-01 15:15:09 +0200

    Added services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,113 @@
 import React from "react";
 import "./services.css";
+import { BsCheck2Square } from "react-icons/bs";
 
 const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services_container">
+        <article className="service">
+          <div className="service_head">
+            <h3>Front End</h3>
+          </div>
+          <ul className="service_list">
+            <li>
+              <BsCheck2Square className="service_list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2Square className="service_list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2Square className="service_list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2Square className="service_list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2Square className="service_list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2Square className="service_list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF FRONT END */}
+        <article className="service">
+          <div className="service_head">
+            <h3>Graphic Design</h3>
+          </div>
+          <ul className="service_list">
+            <li>
+              <BsCheck2Square className="service_list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2Square className="service_list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2Square className="service_list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2Square className="service_list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2Square className="service_list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2Square className="service_list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF GRAPHIC DESIGN */}
+        <article className="service">
+          <div className="service_head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul className="service_list">
+            <li>
+              <BsCheck2Square className="service_list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2Square className="service_list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2Square className="service_list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2Square className="service_list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2Square className="service_list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+            <li>
+              <BsCheck2Square className="service_list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/* END OF CONTENT CREATION */}
+      </div>
+    </section>
+  );
 };
 
 export default Services;
