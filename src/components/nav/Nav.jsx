d22c22faa7607915dc39cec4b20547b75a77aece
Christian SB <christiansb.kodehode@gmail.com>
2022-10-01 12:25:51 +0200

    Setting up navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,61 @@
 import React from "react";
 import "./nav.css";
+import { FaHome } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { FaGraduationCap } from "react-icons/fa";
+import { MdDesignServices } from "react-icons/md";
+import { RiGalleryFill } from "react-icons/ri";
+import { BsChatDotsFill } from "react-icons/bs";
+import { useState } from "react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const [activeNav, setActiveNav] = useState("#");
+  return (
+    <nav>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <FaHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <FaUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <FaGraduationCap />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <MdDesignServices />
+      </a>
+      <a
+        href="#portfolio"
+        onClick={() => setActiveNav("#portfolio")}
+        className={activeNav === "#portfolio" ? "active" : ""}
+      >
+        <RiGalleryFill />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <BsChatDotsFill />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
