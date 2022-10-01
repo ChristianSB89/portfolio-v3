6937f246466137c39fa4694b2a4c5da73c6ca6fd
Christian SB <christiansb.kodehode@gmail.com>
2022-10-02 00:37:37 +0200

    Added contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,65 @@
 import React from "react";
 import "./contact.css";
+import { HiMail } from "react-icons/hi";
+import { BsMessenger } from "react-icons/bs";
+import { BsDiscord } from "react-icons/bs";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <h5>Send meg en</h5>
+      <h2>Melding</h2>
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className="contact_option">
+            <HiMail className="contact_option-icon" />
+            <h4>Email</h4>
+            <h5>christiansandumbreivik@gmail.com</h5>
+            <a href="mailto:christiansandumbreivik@gmail.com" target="_blank">
+              Send en mail
+            </a>
+          </article>
+          <article className="contact_option">
+            <BsMessenger className="contact_option-icon" />
+            <h4>Messenger</h4>
+            <h5>Christian Sandum Breivik</h5>
+            <a href="https://m.me/ChristianSandumBreivik" target="_blank">
+              Send en direktemelding
+            </a>
+          </article>
+          <article className="contact_option">
+            <BsDiscord className="contact_option-icon" />
+            <h4>Discord</h4>
+            <h5>ChristianSB#7845</h5>
+            <a
+              href="https://discord.com/api/webhooks/1025858038831268010/WdF31mdEnoGgD4OGZ_toTOX8YL6Hu26LlrfV4XivtnhN45cG9_SQIv0rYoocCM_ahmyw"
+              target="_blank"
+            >
+              Send meg en melding på serveren
+            </a>
+          </article>
+        </div>
+        <form action="">
+          <input
+            type="text"
+            name="Navn"
+            placeholder="Navn og etternavn"
+            required
+          />
+          <input type="email" name="Email" placeholder="Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Skriv din melding"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send melding
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,68 @@
 import React from "react";
 import "./testimonials.css";
+import SideShot from "../../assets/side-shot.jpg";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
+
+const data = [
+  {
+    avatar: SideShot,
+    name: "Navn Navnesen",
+    review:
+      "I like the rumble of the diesel and the smell of the oil. I percolate my coffee off the radiator boil. I been chasing tuna nearly twentyseven years. I got the eyeballs of an eagle but there is ringing in my ears.",
+  },
+  {
+    avatar: SideShot,
+    name: "Navn Navnesen",
+    review:
+      "I like the rumble of the diesel and the smell of the oil. I percolate my coffee off the radiator boil. I been chasing tuna nearly twentyseven years. I got the eyeballs of an eagle but there is ringing in my ears.",
+  },
+  {
+    avatar: SideShot,
+    name: "Navn Navnesen",
+    review:
+      "I like the rumble of the diesel and the smell of the oil. I percolate my coffee off the radiator boil. I been chasing tuna nearly twentyseven years. I got the eyeballs of an eagle but there is ringing in my ears.",
+  },
+  {
+    avatar: SideShot,
+    name: "Navn Navnesen",
+    review:
+      "I like the rumble of the diesel and the smell of the oil. I percolate my coffee off the radiator boil. I been chasing tuna nearly twentyseven years. I got the eyeballs of an eagle but there is ringing in my ears.",
+  },
+];
 
 const Testimonials = () => {
-  return <section id="testimonials">Testimonials</section>;
+  return (
+    <section id="testimonials">
+      <h5>Hva klientene sier</h5>
+      <h2>Tilbakemeldinger</h2>
+
+      <Swiper
+        pagination={{
+          dynamicBullets: true,
+        }}
+        modules={[Pagination]}
+        spaceBetween={50}
+        slidesPerView={1}
+        className="container testimonials_container"
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client_avatar">
+                <img src={avatar} alt={name} />
+              </div>
+              <h5 className="client_name">{name}</h5>
+              <small className="client_review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
 };
 
 export default Testimonials;
